fe4f3e68e51dc9637f6c47be1d79ae4f14e43dcd
asksa1256@gmail.com <asksa1256@gmail.com>
2025-05-13 19:19:23 +0900

    ReviewForm 리뷰 등록, POST 요청 추가

--- src/api.js
+++ src/api.js
@@ -1,13 +1,27 @@
+const BASE_URL = "https://learn.codeit.kr/api";
+
 export async function getReviews({ order = "rating", offset = 0, limit = 10 }) {
   const query = `order=${order}&offset=${offset}&limit=${limit}`;
-  const response = await fetch(
-    `https://learn.codeit.kr/api/film-reviews?${query}`
-  );
+  const response = await fetch(`${BASE_URL}/film-reviews?${query}`);
 
   if (!response.ok) {
     throw new Error("리뷰를 불러오지 못했어요!");
   }
 
-  const body = response.json();
+  const body = await response.json();
+  return body;
+}
+
+export async function createReview(reviewData) {
+  const response = await fetch(`${BASE_URL}/film-reviews`, {
+    method: "POST",
+    body: reviewData,
+  });
+
+  if (!response.ok) {
+    throw new Error("리뷰를 등록하지 못했어요!");
+  }
+
+  const body = await response.json();
   return body;
 }
--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -2,14 +2,19 @@ import { useState } from "react";
 import "./ReviewForm.css";
 import FileInput from "./FileInput";
 import RatingInput from "./RatingInput";
+import { createReview } from "../api";
+
+const INITIAL_VALUES = {
+  title: "",
+  rating: 0,
+  content: "",
+  imgFile: null,
+};
 
 function ReviewForm() {
-  const [values, setValues] = useState({
-    title: "",
-    rating: 0,
-    content: "",
-    imgFile: null,
-  });
+  const [values, setValues] = useState(INITIAL_VALUES);
+  const [isSubmitting, setIsSubmitting] = useState(false); // 동일한 전송 여러번 하지 않도록 로딩 추가
+  const [submitError, setSubmitError] = useState(null); // 전송 에러 제어
 
   // handleInputChange: 제어 컴포넌트용 handleChange
   // file input 비제어 컴포넌트화 과정에서 handleChange => handleChange, handleInputChange로 추상화 추가
@@ -27,9 +32,24 @@ function ReviewForm() {
     }));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // console.log(values);
+    const formData = new FormData(); // 이 프로젝트에서 사용되는 api가 파일 업로드를 편하게 하기 위해 'FormData' 형식을 사용.
+    Object.entries(values).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+
+    try {
+      setSubmitError(null);
+      setIsSubmitting(true);
+      await createReview(formData);
+      setValues(INITIAL_VALUES);
+    } catch (e) {
+      setSubmitError(e);
+      return;
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   return (
@@ -50,7 +70,10 @@ function ReviewForm() {
         value={values.content}
         onChange={handleInputChange}
       />
-      <button type="submit">확인</button>
+      <button type="submit" disabled={isSubmitting}>
+        확인
+      </button>
+      {submitError?.message && <p>{submitError.message}</p>}
     </form>
   );
 }
